8c20319d31c359b22ed15d7251dd02e665de93b9
josh <joshdfebbie@gmial.com>
2023-12-11 19:17:08 -0700

    Sidebar_fin

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,11 @@
-import { Menu } from 'lucide-react'
 import { UserButton } from '@clerk/nextjs'
 
-import { Button } from '@/components/ui/button'
+import MobileSidabar from '@/components/mobile-sidabar'
 
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
-        <Button variant="ghost" size="icon"
-        className='md:hidden'>
-            <Menu />
-        </Button>
+        <MobileSidabar />
         <div className='flex w-full justify-end'>
             <UserButton afterSignOutUrl='/' />
         </div>
